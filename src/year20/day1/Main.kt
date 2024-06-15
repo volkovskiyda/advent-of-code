076c73a83e35eb5a65611a4ed31c40c36ad9e724
volkovskiyda <volkovskiyda@gmail.com>
2024-06-15 10:49:22 +0300

    day1

---
+++ src/year20/day1/Main.kt
@@ -0,0 +1,38 @@
+package year20.day1
+
+import year20.readLines
+
+fun main() {
+    val lines = readLines()
+
+    val values = lines.map { it.toInt() }
+
+    println("part 1: ${part1(values)}")
+    println("part 2: ${part2(values)}")
+}
+
+private fun part1(values: List<Int>): Int {
+    var result = 0
+    values.forEachIndexed { index1, value1 ->
+        values.drop(index1 + 1).forEachIndexed { index2, value2 ->
+            if (value1 + value2 == 2020) {
+                result = value1 * value2
+            }
+        }
+    }
+    return result
+}
+
+private fun part2(values: List<Int>): Int {
+    var result = 0
+    values.forEachIndexed { index1, value1 ->
+        values.drop(index1 + 1).forEachIndexed { index2, value2 ->
+            values.drop(index2 + 1).forEach { value3 ->
+                if (value1 + value2 + value3 == 2020) {
+                    result = value1 * value2 * value3
+                }
+            }
+        }
+    }
+    return result
+}
