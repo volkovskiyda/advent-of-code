f0ef57be4d549c4b55f4a970a0f5214ceedb2125
volkovskiyda <volkovskiyda@gmail.com>
2024-06-15 10:49:27 +0300

    day2

---
+++ src/year20/day2/Main.kt
@@ -0,0 +1,30 @@
+package year20.day2
+
+import year20.readLines
+
+class Parsed(line: String) {
+    private val groups: List<String> =
+        Regex("(\\d+)-(\\d+) (\\w): (\\w+)").matchEntire(line)?.groupValues.orEmpty().drop(1)
+
+    val first = groups[0].toInt()
+    val second = groups[1].toInt()
+    val symbol = groups[2][0]
+    val pass = groups[3]
+}
+
+fun main() {
+    val lines = readLines()
+
+    val values = lines.map(::Parsed)
+
+    println("part 1: ${part1(values)}")
+    println("part 2: ${part2(values)}")
+}
+
+private fun part1(values: List<Parsed>): Int = values.count { parsed: Parsed ->
+        parsed.pass.count { it  == parsed.symbol } in parsed.first..parsed.second
+    }
+
+private fun part2(values: List<Parsed>): Int = values.count { parsed: Parsed ->
+    (parsed.pass[parsed.first-1] == parsed.symbol) xor (parsed.pass[parsed.second-1] == parsed.symbol)
+}
