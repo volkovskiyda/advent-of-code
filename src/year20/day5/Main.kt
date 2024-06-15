3f16ff1c9704656434b2eafa3b96774ac9c93d7f
volkovskiyda <volkovskiyda@gmail.com>
2024-06-15 10:49:40 +0300

    day5

---
+++ src/year20/day5/Main.kt
@@ -0,0 +1,19 @@
+package year20.day5
+
+import year20.readLines
+
+fun main() {
+    val lines = readLines()
+
+    val values = lines.map { line ->
+        line.map { char -> if (char == 'B' || char == 'R') "1" else "0" }.joinToString("").toInt(2)
+    }
+
+    println("part 1: ${part1(values)}")
+    println("part 2: ${part2(values)}")
+}
+
+private fun part1(seats: List<Int>): Int = seats.max()
+
+private fun part2(seats: List<Int>): Int =
+    (seats.min()..seats.max()).toList().first { seat -> seat !in seats }
