b1fb292156316bcbe4e954edf00030a68f765c30
volkovskiyda <volkovskiyda@gmail.com>
2024-06-15 19:04:41 +0300

    day6

---
+++ src/year20/day6/Main.kt
@@ -0,0 +1,37 @@
+package year20.day6
+
+import util.readLines
+
+fun main() {
+    val lines = readLines()
+
+    println("part 1: ${part1(lines)}")
+    println("part 2: ${part2(lines)}")
+}
+
+private fun part1(lines: List<String>): Int {
+    var current = ""
+    val values = lines.fold(mutableListOf<String>()) { acc, line ->
+        if (line.isEmpty()) {
+            acc.add(current)
+            current = ""
+        } else current += line
+        acc
+    }
+    values.add(current)
+    return values.sumOf { value -> value.toSet().size }
+}
+
+private fun part2(lines: List<String>): Int {
+    var current = ('a'..'z').toList()
+    val values = lines.fold(mutableListOf<List<Char>>()) { acc, line ->
+        if (line.isEmpty()) {
+            acc.add(current)
+            current = ('a'..'z').toList()
+        } else current = current.intersect(line.toList().toSet()).toList()
+        acc
+    }
+    values.add(current)
+    return values.sumOf { value -> value.size }
+}
+
